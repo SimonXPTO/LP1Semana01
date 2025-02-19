e2aeaf4755482f403c06ca2cabb5ed45bbceebec
Simão Durão <simaodurao@gmail.com>
2025-02-19 15:38:46 +0000

    Added Summary to Main

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,10 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// Program starts here.
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
