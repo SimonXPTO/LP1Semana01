3a0f80f17955104e12d441878e5ec27fc658aad9
Simão Durão <simaodurao@gmail.com>
2025-02-20 00:20:08 +0000

    Added Char Variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -9,9 +9,13 @@ namespace VariousTypes
         {
             int firstNumber = 4;
             int secondNumber = 1;
+            char firstChar = '\u00A9';
+            char secondChar = '\uF0A9';
 
             Console.WriteLine(firstNumber);
             Console.WriteLine(secondNumber);
+            Console.WriteLine(firstChar);
+            Console.WriteLine(secondChar);
         }
     }
 }
