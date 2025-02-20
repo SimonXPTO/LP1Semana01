b23ed0ac5ba8057fd218fc5cc657e8a3aee07863
Simão Durão <simaodurao@gmail.com>
2025-02-20 16:08:20 +0000

    Arranged code

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -14,20 +14,26 @@ namespace VariousTypes
         {
             int firstNumber = 4;
             int secondNumber = 1;
+
             char firstChar = '\u00A9';
             char secondChar = '\uF0A9';
+
             float firstfloat = 1.1234f;
             double firstdouble = 5.99;
             long firstlong=8354610239563L;
+
             bool condition= 5<3;
             bool tCond=14.33<15.33;
 
             Console.WriteLine(firstNumber);
             Console.WriteLine(secondNumber);
+
             Console.WriteLine(firstChar);
             Console.WriteLine(secondChar);
+
             Console.WriteLine(firstdouble);
             Console.WriteLine(firstfloat);
+            
             Console.WriteLine(firstlong);
             Console.WriteLine(condition);
             Console.WriteLine(tCond);
