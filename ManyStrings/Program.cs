df377dee679092bd215c3661aed2ce8970dbb5d7
Simão Durão <simaodurao@gmail.com>
2025-02-20 16:51:29 +0000

    Added xx and ii com formatação

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -10,21 +10,33 @@ namespace ManyStrings
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+
+            string xx1 = $"{xx:f2}, {xx:p1}";
+            string ii1= $"{ii:x} e {ii:c}";
+
             string Ola = "Ola pessoal!";
             string nomeIdade = "Eu sou o Simão \nTenho 19 anos\u0021";
+
             string x = "a" + 2;
             string conca1 = "abc" + x;
+
             int a= 3;
             int b= 2;
+
             string Inter = $"valor de x é {a}";
             string Interpol = $"{a} mais {b} é igual a {a+b}";
 
 
-
+            Console.WriteLine(xx1);
+            Console.WriteLine(ii1);
             Console.WriteLine(Ola);
             Console.WriteLine(nomeIdade);
+
             Console.WriteLine(x);
             Console.WriteLine(conca1);
+
             Console.WriteLine(Inter);
             Console.WriteLine(Interpol);
 
