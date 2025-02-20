9e196728e5306c7dc3dfd04fb0e424039d7674d7
Simão Durão <simaodurao@gmail.com>
2025-02-20 16:18:52 +0000

    Added ManyStrings Projects

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        /// <summary>
+        /// Program starts here and presents myself with some string variables
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            string Ola = "Ola pessoal!";
+            string nomeIdade = "Eu sou o Simão \nTenho 19 anos!";
+            Console.WriteLine(Ola);
+            Console.WriteLine(nomeIdade);
+        }
+    }
+}
