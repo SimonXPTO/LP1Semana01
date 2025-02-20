2d348723aa1321d29ba9c32d3c799769d0401fc0
Simão Durão <simaodurao@gmail.com>
2025-02-20 16:33:34 +0000

    Added Concatenation and Interpolation variables

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -12,8 +12,22 @@ namespace ManyStrings
         {
             string Ola = "Ola pessoal!";
             string nomeIdade = "Eu sou o Simão \nTenho 19 anos\u0021";
+            string x = "a" + 2;
+            string conca1 = "abc" + x;
+            int a= 3;
+            int b= 2;
+            string Inter = $"valor de x é {a}";
+            string Interpol = $"{a} mais {b} é igual a {a+b}";
+
+
+
             Console.WriteLine(Ola);
             Console.WriteLine(nomeIdade);
+            Console.WriteLine(x);
+            Console.WriteLine(conca1);
+            Console.WriteLine(Inter);
+            Console.WriteLine(Interpol);
+
         }
     }
 }
