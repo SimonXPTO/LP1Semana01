2b6fe0541e4926b43b171f87d3062425b709a28c
Simão Durão <simaodurao@gmail.com>
2025-02-20 00:10:45 +0000

    Added Int Variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            int firstNumber = 4;
+            int secondNumber = 1;
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+        }
+    }
+}
