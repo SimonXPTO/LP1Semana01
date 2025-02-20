4dd76840fedfcdd60d5b3edaff96905f14b5b194
Simão Durão <simaodurao@gmail.com>
2025-02-20 17:02:19 +0000

    Added Conversion Str to Int

---
+++ AskForInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insera um numero inteiro.");
+
+            string str = Console.ReadLine();
+            int x = int.Parse(str); //Conversão de str para int
+
+
+        }
+    }
+}
