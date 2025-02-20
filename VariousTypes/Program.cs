7c0ad3c6a934575c49301af2873a376266370e84
Simão Durão <simaodurao@gmail.com>
2025-02-20 00:24:16 +0000

    Added Reais

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -11,11 +11,17 @@ namespace VariousTypes
             int secondNumber = 1;
             char firstChar = '\u00A9';
             char secondChar = '\uF0A9';
+            float firstfloat = 1.1234f;
+            double firstdouble = 5.99;
+            long firstlong=8354610239563L;
 
             Console.WriteLine(firstNumber);
             Console.WriteLine(secondNumber);
             Console.WriteLine(firstChar);
             Console.WriteLine(secondChar);
+            Console.WriteLine(firstdouble);
+            Console.WriteLine(firstfloat);
+            Console.WriteLine(firstlong);
         }
     }
 }
