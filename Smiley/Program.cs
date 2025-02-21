c3a7149a52d7d0d93fbdbb4cff3b32b3ad449214
Simão Durão <simaodurao@gmail.com>
2025-02-21 18:02:41 +0000

    Convert UTF8

--- Smiley/Program.cs
+++ Smiley/Program.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using System.Text;
 
 namespace Smiley
 {
     public class Program
     {
+        /// <summary>
+        /// Program converts every Hexadecimal Number to a Unicode Character
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
+            Console.OutputEncoding= Encoding.UTF8;
 
             Console.WriteLine("Insera um codigo Hexadecimal");
+
             string x = Console.ReadLine();
+            int b = int.Parse(x, System.Globalization.NumberStyles.HexNumber);
+            
+            string c = char.ConvertFromUtf32(b);
+            Console.WriteLine($"Unicode:{c}");
+
 
 
         }
